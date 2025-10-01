ec3eebc63243d1c581ab9f15f5ed109aaaa50cc6
Zeke Dou <zeke@zekedou.live>
2025-10-01 21:45:20 +0800

    feat(assets): add TailindCSS

--- assets/vite.config.js
+++ assets/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite"
 import combo from "vite-plugin-combo"
+import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
   plugins: [
@@ -7,5 +8,6 @@ export default defineConfig({
       input: ["src/css/app.css", "src/js/app.js"],
       staticDir: "../priv/static",
     }),
+    tailwindcss()
   ],
 })
