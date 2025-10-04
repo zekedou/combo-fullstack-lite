37750b7984c036cfaa35cb7f3b63a21f9659ed98
Zeke Dou <zeke@zekedou.live>
2025-10-04 23:31:17 +0800

    deps: upgrade combo and combo_vite

--- lib/my_app/pub_sub.ex
+++ lib/my_app/pub_sub.ex
@@ -5,6 +5,6 @@ defmodule MyApp.PubSub do
 
   @spec child_spec(keyword) :: Supervisor.child_spec()
   def child_spec(_) do
-    Phoenix.PubSub.child_spec(name: __MODULE__)
+    Combo.PubSub.child_spec(name: __MODULE__)
   end
 end
